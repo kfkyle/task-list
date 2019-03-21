dcefea866e37f9368ee494533cf7a7dc76b53d0e
Kyle Forbes <kfkyleforbes@gmail.com>
2019-03-20 20:30:11 -0700

    init

---
+++ app.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+    
+    form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTasks);
+
+}
+
+// Add task 
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a');
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fas fa-times"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+
+    taskInput.value = ''
+    e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+
+// Clear Tasks
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
